6e35872a11b8eeaadea343e98493ed8bbf37fbbe
Nathan Ensmenger <nathan.ensmenger@gmail.com>
2018-02-07 12:07:20 -0500

    improved html version

--- make.sh
+++ make.sh
@@ -2,4 +2,4 @@
 
 # this script runs LaTex and more important reminds me to generate an updated html version
 latexmk info590-spring2017.tex
-bibtex2html -nokeywords -noabstract tech-gender.bib
+bibtex2html -t "Technology & Gender Bibliography" -nokeywords -noabstract tech-gender.bib
